59e9b465c1632d8a2df50093f144c3e030c1f342
Erin Stansbury <ekstansbury@gmail.com>
2020-08-15 00:59:25 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+  
+  @@items = []
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)    
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+      item = @@items.find{|i| i.name == item_name}      
+      
+    elsif
+    
+    end
+  end
+end
